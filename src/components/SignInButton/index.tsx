afb2beccecc337c269659e872c55feada13b1e08
Eduarda <eduardapinheiro1302@gmail.com>
2021-12-20 10:16:23 -0300

    feat: creates signinbutton component

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,16 @@
+import { FaGithub } from 'react-icons/fa'
+
+import styles from './styles.module.scss';
+
+export function SignInButton() {
+  return (
+    <button 
+      type="button"
+      className={styles.signInButton}
+    >
+      <FaGithub color="#eba417"/>
+
+      Sign in with Github
+    </button>
+  )
+}
