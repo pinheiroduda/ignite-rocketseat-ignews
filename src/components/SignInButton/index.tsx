3e65d78ccb175e99f4c5346a88d011bed4cc9e5e
Eduarda <eduardapinheiro1302@gmail.com>
2021-12-20 10:23:16 -0300

    feat: add close icon to github button

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,10 +1,10 @@
 import { FaGithub } from 'react-icons/fa'
+import { FiX} from 'react-icons/fi'
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
   const isUserLoggedIn = true;
-
 
   return isUserLoggedIn ? (
     <button 
@@ -12,8 +12,8 @@ export function SignInButton() {
       className={styles.signInButton}
     >
       <FaGithub color="#04d361"/>
-
       Eduarda Pinheiro
+      <FiX color="#737380" className={styles.closeIcon}/>
     </button>
   ) : (
     <button 
