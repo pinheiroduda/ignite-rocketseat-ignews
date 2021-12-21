dcaa1550b7030ff82978275e6f931d81c953e6c4
Eduarda <eduardapinheiro1302@gmail.com>
2021-12-21 16:43:18 -0300

    feat: creates user authentication with github using next auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,24 +1,28 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX} from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
+  console.log(session);
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={()=> signOut()}
     >
       <FaGithub color="#04d361"/>
-      Eduarda Pinheiro
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon}/>
     </button>
   ) : (
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417"/>
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
 import { AppProps } from 'next/app';
 import { Header } from '../components/Header';
+import { SessionProvider as NextAuthPRovider } from "next-auth/react"
 
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthPRovider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthPRovider>
   )
 }
 
